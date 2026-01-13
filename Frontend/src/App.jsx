440866e7293cd73d3fdd83c2eb450aa68d1d6b48
Arya Swaroop <aryaswaroop027@gmail.com>
2026-01-13 15:29:18 +0530

    change the favicon

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+// Lazy load pages
+const Home = lazy(() => import("./pages/Home"));
+
 function App() {
   return (
-    <>
-      <div className="text-3xl font-bold">Hello</div>
-    </>
-  )
-};
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
 
 export default App;
